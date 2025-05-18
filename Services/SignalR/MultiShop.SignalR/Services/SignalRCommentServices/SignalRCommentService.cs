80142ed298076761c8e824f6f66a26f0ba424762
hasanakcan <hasanakcann@gmail.com>
2025-05-18 05:35:50 +0300

    signalr comment and message statistic

--- Services/SignalR/MultiShop.SignalR/Services/SignalRCommentServices/SignalRCommentService.cs
+++ Services/SignalR/MultiShop.SignalR/Services/SignalRCommentServices/SignalRCommentService.cs
@@ -1,17 +1,21 @@
-﻿namespace MultiShop.SignalR.Services.SignalRCommentServices;
+﻿using Newtonsoft.Json;
+
+namespace MultiShop.SignalR.Services.SignalRCommentServices;
 
 public class SignalRCommentService : ISignalRCommentService
 {
-    private readonly HttpClient _httpClient;
-    public SignalRCommentService(HttpClient httpClient)
+    private readonly IHttpClientFactory _httpClientFactory;
+    public SignalRCommentService(IHttpClientFactory httpClientFactory)
     {
-        _httpClient = httpClient;
+        _httpClientFactory = httpClientFactory;
     }
 
     public async Task<int> GetTotalCommentCountAsync()
     {
-        var responseMessage = await _httpClient.GetAsync("comments/gettotalcommentcount");
-        var totalCommentCount = await responseMessage.Content.ReadFromJsonAsync<int>();
-        return totalCommentCount;
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("http://localhost:7075/api/CommentStatistics");
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var commentCount = JsonConvert.DeserializeObject<int>(jsonData);
+        return commentCount;
     }
 }
