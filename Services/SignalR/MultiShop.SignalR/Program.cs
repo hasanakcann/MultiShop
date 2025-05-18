a81bce495586a7f25aaeb764e2698d0efc4fe052
hasanakcan <hasanakcann@gmail.com>
2025-05-18 03:55:43 +0300

    signalr and cors registration

--- Services/SignalR/MultiShop.SignalR/Program.cs
+++ Services/SignalR/MultiShop.SignalR/Program.cs
@@ -1,25 +1,41 @@
+using MultiShop.SignalR.Hubs;
+using MultiShop.SignalR.Services.SignalRCommentServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed(_ => true)
+              .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+builder.Services.AddScoped<ISignalRCommentService, SignalRCommentService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
